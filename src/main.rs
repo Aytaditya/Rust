5e70d9f825496d877f79eac72d31bc7464e6b8c3
Aditya Aryan <adityaaryan@Adityas-Laptop.local>
2024-08-02 02:37:44 +0530

    rust basics

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
-struct Deck(){
+#[derive(Debug)] // this is a rust macro that allows us to print the struct in a readable format
+struct Deck{
     cards:Vec<String> // vectors are like arrays and they can grow and shrink in size 
     // rust also has arrays but they are fixed in size
 }
 
 fn main() {
 
-    let deck=Deck{cards:vec![]};
+    // Generating all 52 cards in a deck
+    // list of suits- hearts , spades
+    // list of values - one two three
+
+    // double nested for loop to generate all 52 cards
+
+    
+    let deck=Deck{cards:vec![]}; // creating a new instance of the Deck struct
+    //let deck=Deck{cards:vec!["two of hearts".to_string(),"ace of spades".to_string()]};
     // let deck is a variable known as binding in rust 
 
-
-    println!("Hello, world!");
-    println!("Here is deck:{}",deck);s
+    println!("Here is deck:{:?}",deck);
 }
