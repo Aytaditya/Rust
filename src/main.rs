fd8e8f1083f5ae0d1d30f4f8b8b1444f59ce3ff6
Aditya Aryan <adityaaryan@Adityas-Laptop.local>
2024-08-02 03:32:04 +0530

    pushed combination of cards in vector

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,33 @@ struct Deck{
 
 fn main() {
 
+    // declaration of array 
+    let arr=[1,2,3,4,5];  // array is fixed and cannot grow or shrink in size
+
+
     // Generating all 52 cards in a deck
+
     // list of suits- hearts , spades
+    //let suits=vec!["hearts","spades","clubs"]; // vector of strings
+    let suits=["hearts","spades","clubs","diamonds"]; // array of strings
+
     // list of values - one two three
+    //let values=vec!["one","two","three"]; // vector of strings
+    let values=["one","two","three"]; // array of strings
+
+    let mut cards=vec![]; // empty vector of cards 
+    // by default it is immutable so we need to make it mutable by using mut keyword
 
     // double nested for loop to generate all 52 cards
+    for suit in suits{
+        for value in values{
+            let card=format!("{} of {}",value,suit); // format! is a macro that allows us to format strings
+            cards.push(card); // push the card into the cards vector
+        }
+    }
 
-    
+    println!("Here are the cards:{:?}",cards);
+
     let deck=Deck{cards:vec![]}; // creating a new instance of the Deck struct
     //let deck=Deck{cards:vec!["two of hearts".to_string(),"ace of spades".to_string()]};
     // let deck is a variable known as binding in rust 
